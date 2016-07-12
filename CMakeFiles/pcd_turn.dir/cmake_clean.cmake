1d43a5243fe66bb6916d1e7b1644dfb1e8808edf
tomas987 <2210291tt@gmail.com>
2016-07-12 14:17:29 +0900

    PCL

---
+++ CMakeFiles/pcd_turn.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pcd_turn.dir/pcd_turn.cpp.o"
+  "pcd_turn.pdb"
+  "pcd_turn"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pcd_turn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ CMakeFiles/pcd_view.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pcd_view.dir/pcd_view.cpp.o"
+  "pcd_view.pdb"
+  "pcd_view"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pcd_view.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
